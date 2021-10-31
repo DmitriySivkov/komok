ef5c7a9b7646d40cca208d9a322d56e16fdf9d01
Dmitrii Sivkov <sivkovdw@yandex.ru>
2021-10-31 16:51:57 +0300

    blocks on main + readme

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MainBlock;
 use App\Models\WebsiteCommonSetting;
 use Illuminate\Http\Request;
 
@@ -9,9 +10,15 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $data = WebsiteCommonSetting::all()->firstOrFail();
+        $data['website_common_settings'] = WebsiteCommonSetting::all()->firstOrFail();
+        $data['blocks_on_main'] = MainBlock::all()->keyBy('id');
+        $data['blocks_on_main']->each(function($item) {
+            if ($item->emphasized_text)
+                $item->headline = str_replace($item->emphasized_text, '', $item->headline);
+        });
         return view('home', [
-            'website_common_settings' => $data
+            'website_common_settings' => $data['website_common_settings'],
+            'blocks_on_main' => $data['blocks_on_main']
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,18 @@
         <div class="grid-x">
             <div class="cell large-9 large-order-1 small-order-2">
                 <div class="section__content">
-                    <div class="section__head"><span class="section__violet section__violet_1">Лагерь,</span>&nbsp;в который хочется
-                        возвращаться снова и снова
+                    <div class="section__head">
+                        <span class="section__violet section__violet_1">{{ $blocks_on_main[1]->emphasized_text }}</span>
+                        {{ $blocks_on_main[1]->headline }}
                     </div>
-                    <div class="section__text"><p>Комок ― это молодежный лагерь с уникальной атмосферой, в которой каждый чувствует
-                            себя комфортно.</p>
-                        <p>Новые друзья, миллион полезных навыков, вагон положительных эмоций и фотографий — вот что получит ваш
-                            ребенок после посещения «Комка»</p></div>
-                    <div><a class="button" href="#">Подробнее о сменах</a></div>
+                    <div class="section__text">
+                        {!! $blocks_on_main[1]->description !!}
+                    </div>
+                    <div><a class="button" href="{{ $blocks_on_main[1]->button_link }}">{{ $blocks_on_main[1]->button_title }}</a></div>
                 </div>
             </div>
             <div class="cell large-13 large-offset-2 section__background large-order-2 small-order-1">
-                <img class="section__slide" src="{{ asset('i/komok-main-kid_1.jpg') }}">
+                <img class="section__slide" src="{{ asset('storage/' . $blocks_on_main[1]->picture) }}">
                 <img class="section__img section__img_1" src="{{ asset('images/mainpage/back1/Group.png') }}">
                 <img class="section__img section__img_2" src="{{ asset('images/mainpage/back1/Group-1.png') }}">
                 <img class="section__img section__img_3" src="{{ asset('images/mainpage/back1/Group-2.png') }}">
@@ -33,10 +33,11 @@
                 <div class="grid-x">
                     <div class="cell large-11 large-offset-6">
                         <div class="section__content section__content_center">
-                            <div class="section__head">Каждая смена&thinsp;<span
-                                    class="section__violet section__violet_2">неповторима</span></div>
-                            <div class="section__text">Мы верим, что каждый ребенок талантлив и помогаем каждому раскрыть свой потенциал
-                                благодаря сменам с уникальными тематиками.
+                            <div class="section__head">
+                                {{ $blocks_on_main[2]->headline }}<span class="section__violet section__violet_2">{{ $blocks_on_main[2]->emphasized_text }}</span>
+                            </div>
+                            <div class="section__text">
+                                {!! $blocks_on_main[2]->description !!}
                             </div>
                         </div>
                     </div>
@@ -50,17 +51,18 @@
         <div class="section__background">
             <div class="grid-x">
                 <div class="cell large-9 large-offset-2">
-                    <img class="section__slide" src="{{ asset('i/komok-main-kid_2.jpg') }}">
+                    <img class="section__slide" src="{{ asset('storage/' . $blocks_on_main[3]->picture) }}">
                 </div>
                 <div class="cell large-9">
                     <div class="section__content">
                         <div class="section__head">
-                            <div class="section__suptop">7-12 лет</div>
-                            Смены&thinsp;<span class="section__violet section__violet_3">для детей</span></div>
-                        <div class="section__text"><p>Развиваем навыки самопрезентации и ораторского мастерства, повышаем уровень
-                                общей эрудициии самодисциплины, закрепляем ранее приобретенные знания английского языка, и все это в игровой
-                                непринужденной форме.</p></div>
-                        <div><a class="button" href="#">Выбрать смену</a></div>
+                            <div class="section__suptop">{{ $blocks_on_main[3]->text_above_headline }}</div>
+                            {{ $blocks_on_main[3]->headline }}<span class="section__violet section__violet_3">{{ $blocks_on_main[3]->emphasized_text }}</span>
+                        </div>
+                        <div class="section__text">
+                            {!! $blocks_on_main[3]->description !!}
+                        </div>
+                        <div><a class="button" href="{{ $blocks_on_main[3]->button_link }}">{{ $blocks_on_main[3]->button_title }}</a></div>
                     </div>
                 </div>
             </div>
@@ -74,17 +76,16 @@
                     <div class="cell large-10 large-offset-2 large-order-1 small-order-2">
                         <div class="section__content">
                             <div class="section__head">
-                                <div class="section__suptop">13-17 лет</div>
-                                Смены&thinsp;<span class="section__violet section__violet_4">для подростков</span></div>
-                            <div class="section__text"><p>Обучающие и спортивные игры, психологические тренинги, направленные на
-                                    гармонизацию отношений в мини-коллективе, целеполагание, развитие лидерских навыков (приоритизация,
-                                    осознанность, умение взять ответственность), индивидуальные и групповые задания для раскрытия творческого
-                                    потенциала.</p></div>
-                            <div><a class="button" href="#">Выбрать смену</a></div>
+                                <div class="section__suptop">{{ $blocks_on_main[4]->text_above_headline }}</div>
+                                {{ $blocks_on_main[4]->headline }}<span class="section__violet section__violet_4">{{ $blocks_on_main[4]->emphasized_text }}</span></div>
+                            <div class="section__text">
+                                {!! $blocks_on_main[4]->description !!}
+                            </div>
+                            <div><a class="button" href="{{ $blocks_on_main[4]->button_link }}">{{ $blocks_on_main[4]->button_title }}</a></div>
                         </div>
                     </div>
                     <div class="cell large-11 large-offset-1 large-order-2 small-order-1">
-                        <img class="section__slide" src="{{ asset('i/komok-main-kid_3.jpg') }}">
+                        <img class="section__slide" src="{{ asset('storage/' . $blocks_on_main[4]->picture) }}">
                     </div>
                 </div>
                 <img class="section__img section__img_12" src="{{ asset('images/mainpage/back4/Brain.png') }}">
@@ -98,8 +99,8 @@
         <div class="grid-container">
             <div class="board__wrapper">
                 <img class="board__img" src="{{ asset('images/mainpage/back5/circle.png') }}">
-                <div class="board__content">Путевка в наш лагерь — это выгодная инвестиция в развитие вашего ребенка! Мы учим
-                    отдыхать и дружить, верить в себя и стремиться к цели.
+                <div class="board__content">
+                    {!! $blocks_on_main[5]->description !!}
                 </div>
             </div>
         </div>
@@ -109,17 +110,18 @@
             <div class="grid-x">
                 <div class="cell large-9 large-order-1 small-order-2">
                     <div class="section__content">
-                        <div class="section__head"><span class="section__violet section__violet_5">Больше,</span>&nbsp;чем просто
-                            лагерь
+                        <div class="section__head">
+                            <span class="section__violet section__violet_5">{{ $blocks_on_main[6]->emphasized_text }}</span>
+                            {{ $blocks_on_main[6]->headline }}
                         </div>
-                        <div class="section__text"><p>Уникальная атмосфера, талантливые вожатые и незабываемые эмоции — только часть
-                                причин, почему ваш ребенок захочет возвращаться в Комок каждое лето.Но причин так много, что нам пришлось
-                                сделать отдельную страницу, чтобы рассказать вам о них.</p></div>
-                        <div><a class="button" href="#">Узнать все о лагере</a></div>
+                        <div class="section__text">
+                            {!! $blocks_on_main[6]->description !!}
+                        </div>
+                        <div><a class="button" href="{{ $blocks_on_main[6]->button_link }}">{{ $blocks_on_main[6]->button_title }}</a></div>
                     </div>
                 </div>
                 <div class="cell large-13 large-offset-2 section__background large-order-2 small-order-1">
-                    <img class="section__slide" src="{{ asset('i/komok-main-kid_4.jpg') }}">
+                    <img class="section__slide" src="{{ asset('storage/' . $blocks_on_main[6]->picture) }}">
                     <img class="section__img section__img_16" src="{{ asset('images/mainpage/back6/Brain.png') }}">
                     <img class="section__img section__img_17" src="{{ asset('images/mainpage/back6/Clock.png') }}">
                     <img class="section__img section__img_18" src="{{ asset('images/mainpage/back6/Mask.png') }}">
