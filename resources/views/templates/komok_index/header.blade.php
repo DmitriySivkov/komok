af8198e2ab9c17b2f17d3bbfc726774a0aa34799
Kirill Sasyk <unknownsomebody@yandex.ru>
2022-03-31 15:20:35 +0300

    change layout header and delete some working wrong stuff

--- resources/views/templates/komok_index/header.blade.php
+++ resources/views/templates/komok_index/header.blade.php
@@ -2,7 +2,20 @@
     <header class="header">
         <a class="header__logo" href="/" title="На главную">
             <img src="/storage/{{ json_decode($settings['site.logo']->value, true)[0]['download_link']}}">
+        <button class="burger" id="navToggle" type="button">
+            <span class="burger__item">Menu</span>
+        </button>
         <nav class="nav" id="nav">
+            <div class="work-time">
+                <div class="work-time__wrapper">
+                    <div class="work-time__item">
+                        <a class="work-time__link" href="tel:+{{ preg_replace('/[^0-9.]+/', '', $settings["site.main_phone"]->value) }}">
+                            {{ $settings["site.main_phone"]->value }}
+                        </a>
+                    </div>
+                    <div class="work-time__item">{{ $settings["site.work_time"]->value }}</div>
+                </div>
+            </div>
             <div class="nav__links">
                 {{ menu('Главное', 'components.menu.main_top') }}
                 {{ menu('Главное 2', 'components.menu.main_top_second') }}
@@ -17,9 +30,6 @@
                 </div>
                 <div class="work-time__item">{{ $settings["site.work_time"]->value }}</div>
             </div>
-            <button class="burger" id="navToggle" type="button">
-                <span class="burger__item">Menu</span>
-            </button>
         </div>
     </header>
 </div>
